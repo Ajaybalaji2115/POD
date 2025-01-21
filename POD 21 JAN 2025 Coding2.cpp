89f2b714d44bfb6d1e79cba6d0b9979c8084ae05
Ajaybalaji2115 <150769063+Ajaybalaji2115@users.noreply.github.com>
2025-01-21 23:14:12 +0530

    Create POD 21 JAN 2025 Coding2.cpp

---
+++ POD 21 JAN 2025 Coding2.cpp
@@ -0,0 +1,47 @@
+/*Single File Programming Question
+Problem Statement
+
+
+
+Sam is a high school student who loves solving puzzles and challenges in his computer science class. One day, his teacher gave him an interesting problem related to palindromes. The task is to find the shortest palindrome that can be created by adding characters only to the beginning of a given string.
+
+
+
+Given a string s, Sam needs to write a program to find the shortest palindrome that can be formed by adding characters only to the beginning of the string using KMP algorithm.
+
+Input format :
+The input consists of a string, s.
+
+Output format :
+The output prints the shortest palindrome that can be formed by adding characters to the beginning of the string s.
+
+
+
+Refer to the sample output for formatting specifications.
+
+Code constraints :
+2 < s.length ≤ 100
+
+The string consists of lowercase English letters only.
+
+Sample test cases :
+Input 1 :
+aacecaaa
+Output 1 :
+aaacecaaa
+Input 2 :
+abcd
+Output 2 :
+dcbabcd
+Note :
+The program will be evaluated only after the “Submit Code” is clicked.
+Extra spaces and new line characters in the program output will result in the failure of the test case.
+Marks : 10
+Negative Marks : 0
+image
+Fill your code here
+Python (3.8)
+theme
+instruction
+
+Clear*/
